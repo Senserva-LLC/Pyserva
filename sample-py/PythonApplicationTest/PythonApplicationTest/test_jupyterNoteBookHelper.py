e7a04bf128b1ecac8c494021b140dc27f9bf08c8
Cheng-Lung Wang <Cheng@senserva.com>
2021-10-29 10:08:30 -0500

    1st unit test

--- sample-py/PythonApplicationTest/PythonApplicationTest/test_jupyterNoteBookHelper.py
+++ sample-py/PythonApplicationTest/PythonApplicationTest/test_jupyterNoteBookHelper.py
@@ -1,10 +1,27 @@
 import unittest
-import Pyserva
-from Pyserva import JupyterNotebookHelper as Senserva
+#1
+#import Pyserva
+#from Pyserva import JupyterNotebookHelper as Senserva
+
+#2
+#import sys
+#sys.path.append('C:\\Users\\Administrator\\Source\\Repos\\Senserva-LLC\\Pyserva\\src\\Pyserva')
+#import JupyterNotebookHelper as test
+
+#3
+import importlib.util
 
 class Test_test_jupyterNoteBookHelper(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+    def test_read_config_values(self):
+        spec = importlib.util.spec_from_file_location("jupyterNoteBookHelper",
+                                                      "C:\\Users\\Administrator\\Source\\Repos\\Senserva-LLC\\Pyserva\\src\\PyservajupyterNoteBookHelper.py")
+        test = importlib.util.module_from_spec(spec)
+
+        try:           
+            rtn = test.read_config_values()
+        except Exception:        
+            pass  
+     
 
 if __name__ == '__main__':
     unittest.main()
